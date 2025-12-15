df064f95541b27395259b00471ba9f2bca120c47
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-12-15 09:40:48 -0800

    preped for render deployment

--- server/src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  databaseUrl:
-    "postgresql://neondb_owner:npg_7alfmN1VAEWd@ep-bold-heart-a4qo8zki-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-};
-
-export default config;
