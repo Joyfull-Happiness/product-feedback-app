9e7cb1fd8b760099b394f6ec739e3821085a354a
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-12-15 09:58:30 -0800

    updated render error

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,7 @@ import pg from "pg"; // pg stands for PostgreSQL, for connecting to the database
 //connecting to our PostgreSQL database, or db for short
 const db = new pg.Pool({
   // new pg.Pool() creates a connection to the database
-  connectionString: process.env.DATABASE.URL, // credentials to access the database. Keep private!
+  connectionString: process.env.DATABASE_URL, // credentials to access the database. Keep private!
   ssl: true, // use SSL encryption when connecting to the database to keep data safe
 });
 
