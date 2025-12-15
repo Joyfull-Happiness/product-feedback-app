95f63174e126b2258c1f1ae21ec8212cf24d4bb1
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-12-15 11:22:55 -0800

    updated feedback css bug

--- client/src/Pages/Feedback.jsx
+++ client/src/Pages/Feedback.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-import "./feedback.css";
+import "./Feedback.css";
 
 export default function Feedback() {
   const emptyFormState = {
